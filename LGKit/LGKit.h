1a49102a911ca88043004bb33f5f73296d4fd290
arrcen-lg <515930926@qq.com>
2018-02-07 16:52:58 +0800

    init

---
+++ LGKit/LGKit.h
@@ -0,0 +1,19 @@
+//
+//  LGKit.h
+//  LGKit
+//
+//  Created by iOS on 2018/2/7.
+//  Copyright © 2018年 lg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LGKit.
+FOUNDATION_EXPORT double LGKitVersionNumber;
+
+//! Project version string for LGKit.
+FOUNDATION_EXPORT const unsigned char LGKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LGKit/PublicHeader.h>
+
+
